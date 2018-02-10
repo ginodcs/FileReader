253a3ed8d30f3ed7561347ed35ce7baef420bd61
g.del.carmen.suero <g.del.carmen.suero@WIN-STUHBONSUV>
2018-02-10 19:52:24 +0100

    Agregar archivos de proyecto.

---
+++ FileReader/FileManager/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    public class FileManager : IFileManager
+    {
+        public string ReadFile(string pathFile)
+        {
+            string text;
+
+            var fileStream = new FileStream(pathFile, FileMode.Open, FileAccess.Read);
+
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                text = streamReader.ReadToEnd();
+            }
+
+            return text;
+        }
+    }
+}
---
+++ FileReader/FileManager/IFileManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    public interface IFileManager
+    {
+        /// <summary>
+        /// Read a text file
+        /// </summary>
+        /// <param name="pathFile">return the file as string</param>
+        /// <returns></returns>
+        string ReadFile(string pathFile);
+    }
+}
